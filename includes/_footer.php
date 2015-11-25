e477b09516404645bf65c99cb15663162b39c622
CorkyMaigre <mai.cor@hotmail.com>
2015-11-26 00:13:41 +0100

    Separating the footer html in includes directory

---
+++ includes/_footer.php
@@ -0,0 +1,5 @@
+﻿<div id="footer">
+	<div id="about"><p>HEHLan All Rights Reserved 'Copyright' 2015</p></div>
+	<div id="nothinghere"><img src="img/logo3.png" alt="CEHECOFH"></div>
+	<div id="social"><a href="http://www.heh.be" target="_blank"><img src="img/logo4.png" alt="HeH" border="0"></a></div>
+</div>
