1abedca79961830f4bf684fd5a87d703f4ef8553
CorkyMaigre <mai.cor@hotmail.com>
2015-11-26 19:39:24 +0100

    Code cleaning and detachment

--- common/menuTop.php
+++ common/menuTop.php
@@ -1,18 +1,34 @@
-<?php
 
-echo            '<ul>
-                    <li><a href="index.php">Accueil</a></li>
-                    <li><a href="joueurs.php">Joueurs</a></li>
-                    <li><a href="tournois.php?id=1">LOL</a></li>
-                    <li><a href="tournois.php?id=2">CoD4</a></li>
-                    <li><a href="tournois.php?id=3">TM</a></li>
-					<li><a href="tournois.php?id=5">Hearthstone</a></li>';
-if(isset($_SESSION['id_joueur']))
-{
-	echo '<li><a href="ModifProfil.php">Mon profil</a></li>';
-}						
-if(isset($_SESSION['level']))
-{
-	if($_SESSION['level']==1) echo '<li><a href="admin/index.php">Admin</a></li>';
-}					
-echo '           </ul>';
+<?php 
+
+	//SQL Query to select all tournament names
+	$sql = "SELECT id_tournoi, nomTournoi FROM tournoi";
+	$query = $connexion->prepare($sql);
+	$query->execute();
+	
+	echo '<ul>';
+	echo '<li><a href="index.php">Accueil</a></li>';
+	echo '<li><a href="joueurs.php">Joueurs</a></li>';
+	
+	while($data = $query->fetch(PDO::FETCH_ASSOC))
+	{		
+		echo '<li><a href="tournois.php?id='.$data['id_tournoi'].'">'.$data['nomTournoi'].'</a></li>';
+	}	
+		
+	if(isset($_SESSION['id_joueur']))
+		echo '<li><a href="ModifProfil.php">Mon profil</a></li>';
+
+	if(isset($_SESSION['level']))
+	{
+		if($_SESSION['level'] == 1)
+			echo '<li><a href="admin/index.php">Admin</a></li>';
+	}
+	
+	echo '</ul>';	
+  
+?>
+
+
+
+	
+
