13d3ee2365ba2bb8d9535a676e1aeaff024d67ca
CorkyMaigre <mai.cor@hotmail.com>
2016-03-15 17:30:41 +0100

    Fix urls in require_once

--- joueurs.php
+++ joueurs.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-require_once('/class/var.conf.php');
+require_once('class/var.conf.php');
 require_once(DOCUMENT_ROOT.'/common/utils.php');
 require_once(DOCUMENT_ROOT.'/class/Smarty_HEHLan.class.php');
 require_once(DOCUMENT_ROOT.'/class/Database.class.php');
