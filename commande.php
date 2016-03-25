b92cb74ef02c513735352a4dccea8dd733791ee2
Skit5 <chapeau.plume@gmail.com>
2016-03-25 09:36:49 +0100

    récup de getXhr

--- commande.php
+++ commande.php
@@ -1,6 +1,13 @@
 <?php
 require_once('common/head.php');
 
-
+$commandesList = $database->getJoueurCommandes($_SESSION['id_joueur']);
+foreach($commandesList as $kc=>$commande)
+{
+	$commandesList[$kc]['nom'] = "Commande ".$kc+1;
+	$commandesList[$kc]['date'] = getRelativeTime($commande['date']);
+	$commandesList[$kc]['commande'] = $database->getCommande($commande['id_commande']);
+}
+$smarty->assign('commandes',$commandesList);
 $smarty->display('default/commande.tpl');
 ?>
